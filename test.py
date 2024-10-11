5da7a3dccff4953390c76b799b8b7037e15f44bb
fuyunyou <2152291487@qq.com>
2024-10-11 23:43:03 +0800

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+###
+ # @Author: fuyunyou
+ # @Date: 2024-10-11 23:42:26
+ # @LastEditors: fuyunyou
+ # @LastEditTime: 2024-10-11 23:42:28
+ # @Description: 
+ # @FilePath: \AlienAttack_py\test.py
+###
+print("hello,world!")
