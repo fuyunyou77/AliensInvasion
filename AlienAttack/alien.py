cfdb1684336175c00efe21091fde2403598fb4c3
fuyunyou <2152291487@qq.com>
2024-10-13 23:32:47 +0800

    抠图，让外星人没有白边框

--- AlienAttack/alien.py
+++ AlienAttack/alien.py
@@ -1,3 +1,11 @@
+###
+ # @Author: fuyunyou
+ # @Date: 2024-10-13 22:56:30
+ # @LastEditors: fuyunyou
+ # @LastEditTime: 2024-10-13 23:18:41
+ # @Description: 外星人类:包含外星人的一些基本行为和信息
+ # @FilePath: \ai_game_project\AlienAttack\alien.py
+###
 import pygame
 from pygame.sprite import Sprite
 
@@ -10,7 +18,7 @@ class Alien(Sprite):
         self.screen=ai_game.screen
 
         #加载外星人图像并设置其rect属性
-        self.image=pygame.image.load('images/alien.bmp')
+        self.image=pygame.image.load('../images/alien.bmp')
         self.rect=self.image.get_rect()
         
         #每个外星人最初都在左上角生成
